228089127b44da16399b92934be180d927a6f2d2
omarhossameldin <okandil273@gmail.com>
2022-01-02 19:34:23 +0200

    init-sanctum-model&createing-category&sub-category

---
+++ app/Models/Category.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function parent()
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    /**
+     * Get Categories Tree
+     *
+     * @param object $categories
+     * @return mixed
+     */
+    public static function nestable(object $categories): mixed
+    {
+        foreach ($categories as $category) {
+            if (!$category->children->isEmpty()) {
+                $category->children = self::nestable($category->children);
+            }
+        }
+        return $categories;
+    }
+}
