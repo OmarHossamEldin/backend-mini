5d4ca852ec38a9c06d63401b89c30fb5ad886934
omarhossameldin <okandil273@gmail.com>
2022-01-02 19:30:49 +0200

    adding-translating-for-the-api

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'setHeaderLang' => 'please insert the Content-language header',
+    'register'   => 'Register completed successfully please check your verifcation mail in your inbox',
+    'email'    => 'this email not a real email please write a real one',
+    'mustVerify'    => 'please go to your email for activate it and follow the instructions',
+    'success'   => 'These E-mail and password are right and you logged in successfully',
+    'failed' => 'These E-mail or password do not match.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'logout' => 'you logged out successfully',
+    'unAuthorized' => 'you are not allowed to do this operation'
+
+];
