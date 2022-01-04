136a4102af390523fa82a795a6c6e3a468375b08
omarhossameldin <okandil273@gmail.com>
2022-01-05 00:31:29 +0200

    remove-unneeded-atterbuties

--- database/migrations/2022_01_02_151317_create_categories_table.php
+++ database/migrations/2022_01_02_151317_create_categories_table.php
@@ -16,7 +16,6 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->longText('description')->nullable();
             $table->foreignId('parent_id')->nullable()->constrained('categories');
             $table->timestamps();
         });
